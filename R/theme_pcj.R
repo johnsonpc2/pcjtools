cf19be13c19372af61ce7afd1eb648c3e1530ecd
Pierce Johnson <johnsonpierce.c@gmail.com>
2026-02-16 16:43:50 -0500

    Updated defaults of theme_pcj function to handle manual specification of all plot elements

--- R/theme_pcj.R
+++ R/theme_pcj.R
@@ -51,9 +51,13 @@ theme_pcj <- function(gridlines = TRUE, palette = "default",
     ggplot2::element_blank()
   }
 
-  # Set defaults for all theme parameters (only if not provided by user)
+  # ========== BASE ELEMENTS ==========
   if (!"line" %in% names(dots)) {
     dots$line <- ggplot2::element_line(color = colors[1], linewidth = .5)
+  }
+
+  if (!"rect" %in% names(dots)) {
+    dots$rect <- ggplot2::element_rect(fill = colors[15], color = colors[1], linewidth = 0.5)
   }
 
   if (!"text" %in% names(dots)) {
@@ -62,22 +66,26 @@ theme_pcj <- function(gridlines = TRUE, palette = "default",
       size = 16,
       face = "bold",
       lineheight = 1.2,
-      margin = ggplot2::margin(t = 0, r = 0, b = 5, l = 0, unit = "pt")
+      margin = ggplot2::margin(t = 0, r = 0, b = 5, l = 0, unit = "pt"),
+      color = colors[1]
     )
   }
 
+  # ========== AXIS ELEMENTS ==========
   if (!"axis.line" %in% names(dots)) {
     dots$axis.line <- ggplot2::element_line(color = colors[1], linewidth = 0.5)
   }
 
-  if (!"axis.text" %in% names(dots)) {
-    dots$axis.text <- ggplot2::element_text(size = ggplot2::rel(1.2), color = colors[3])
+  if (!"axis.line.x" %in% names(dots)) {
+    dots$axis.line.x <- NULL  # Inherits from axis.line
   }
 
-  if (!"axis.text.y" %in% names(dots)) {
-    dots$axis.text.y <- ggplot2::element_text(
-      margin = ggplot2::margin(t = 0, r = 5, b = 0, l = 0)
-    )
+  if (!"axis.line.y" %in% names(dots)) {
+    dots$axis.line.y <- NULL  # Inherits from axis.line
+  }
+
+  if (!"axis.text" %in% names(dots)) {
+    dots$axis.text <- ggplot2::element_text(size = ggplot2::rel(1.2), color = colors[3])
   }
 
   if (!"axis.text.x" %in% names(dots)) {
@@ -86,25 +94,88 @@ theme_pcj <- function(gridlines = TRUE, palette = "default",
     )
   }
 
+  if (!"axis.text.x.top" %in% names(dots)) {
+    dots$axis.text.x.top <- NULL  # Inherits from axis.text.x
+  }
+
+  if (!"axis.text.x.bottom" %in% names(dots)) {
+    dots$axis.text.x.bottom <- NULL  # Inherits from axis.text.x
+  }
+
+  if (!"axis.text.y" %in% names(dots)) {
+    dots$axis.text.y <- ggplot2::element_text(
+      margin = ggplot2::margin(t = 0, r = 5, b = 0, l = 0)
+    )
+  }
+
+  if (!"axis.text.y.left" %in% names(dots)) {
+    dots$axis.text.y.left <- NULL  # Inherits from axis.text.y
+  }
+
+  if (!"axis.text.y.right" %in% names(dots)) {
+    dots$axis.text.y.right <- NULL  # Inherits from axis.text.y
+  }
+
   if (!"axis.ticks" %in% names(dots)) {
     dots$axis.ticks <- ggplot2::element_line(color = colors[1], linewidth = 0.5)
+  }
+
+  if (!"axis.ticks.x" %in% names(dots)) {
+    dots$axis.ticks.x <- NULL  # Inherits from axis.ticks
+  }
+
+  if (!"axis.ticks.x.top" %in% names(dots)) {
+    dots$axis.ticks.x.top <- NULL  # Inherits from axis.ticks.x
+  }
+
+  if (!"axis.ticks.x.bottom" %in% names(dots)) {
+    dots$axis.ticks.x.bottom <- NULL  # Inherits from axis.ticks.x
+  }
+
+  if (!"axis.ticks.y" %in% names(dots)) {
+    dots$axis.ticks.y <- NULL  # Inherits from axis.ticks
+  }
+
+  if (!"axis.ticks.y.left" %in% names(dots)) {
+    dots$axis.ticks.y.left <- NULL  # Inherits from axis.ticks.y
+  }
+
+  if (!"axis.ticks.y.right" %in% names(dots)) {
+    dots$axis.ticks.y.right <- NULL  # Inherits from axis.ticks.y
   }
 
   if (!"axis.ticks.length" %in% names(dots)) {
     dots$axis.ticks.length <- ggplot2::unit(3.5, "pt")
   }
 
+  if (!"axis.ticks.length.x" %in% names(dots)) {
+    dots$axis.ticks.length.x <- NULL  # Inherits from axis.ticks.length
+  }
+
+  if (!"axis.ticks.length.x.top" %in% names(dots)) {
+    dots$axis.ticks.length.x.top <- NULL
+  }
+
+  if (!"axis.ticks.length.x.bottom" %in% names(dots)) {
+    dots$axis.ticks.length.x.bottom <- NULL
+  }
+
+  if (!"axis.ticks.length.y" %in% names(dots)) {
+    dots$axis.ticks.length.y <- NULL  # Inherits from axis.ticks.length
+  }
+
+  if (!"axis.ticks.length.y.left" %in% names(dots)) {
+    dots$axis.ticks.length.y.left <- NULL
+  }
+
+  if (!"axis.ticks.length.y.right" %in% names(dots)) {
+    dots$axis.ticks.length.y.right <- NULL
+  }
+
   if (!"axis.title" %in% names(dots)) {
     dots$axis.title <- ggplot2::element_text(
       size = ggplot2::rel(1.5),
       color = colors[1]
-    )
-  }
-
-  if (!"axis.title.y" %in% names(dots)) {
-    dots$axis.title.y <- ggplot2::element_text(
-      angle = 90,
-      margin = ggplot2::margin(t = 0, r = 10, b = 0, l = 0, unit = "pt")
     )
   }
 
@@ -115,32 +186,64 @@ theme_pcj <- function(gridlines = TRUE, palette = "default",
     )
   }
 
-  if (!"legend.box.background" %in% names(dots)) {
-    dots$legend.box.background <- ggplot2::element_rect(linewidth = 1)
+  if (!"axis.title.x.top" %in% names(dots)) {
+    dots$axis.title.x.top <- NULL  # Inherits from axis.title.x
   }
 
-  if (!"legend.box.margin" %in% names(dots)) {
-    dots$legend.box.margin <- ggplot2::margin(t = 5, r = 25, b = 0, l = 25)
+  if (!"axis.title.x.bottom" %in% names(dots)) {
+    dots$axis.title.x.bottom <- NULL  # Inherits from axis.title.x
   }
 
-  if (!"legend.box.spacing" %in% names(dots)) {
-    dots$legend.box.spacing <- ggplot2::unit(6, "pt")
+  if (!"axis.title.y" %in% names(dots)) {
+    dots$axis.title.y <- ggplot2::element_text(
+      angle = 90,
+      margin = ggplot2::margin(t = 0, r = 10, b = 0, l = 0, unit = "pt")
+    )
   }
 
-  if (!"legend.direction" %in% names(dots)) {
-    dots$legend.direction <- "horizontal"
+  if (!"axis.title.y.left" %in% names(dots)) {
+    dots$axis.title.y.left <- NULL  # Inherits from axis.title.y
   }
 
-  if (!"legend.justification" %in% names(dots)) {
-    dots$legend.justification <- c(1, 0)
+  if (!"axis.title.y.right" %in% names(dots)) {
+    dots$axis.title.y.right <- NULL  # Inherits from axis.title.y
+  }
+
+  # ========== LEGEND ELEMENTS ==========
+  if (!"legend.background" %in% names(dots)) {
+    dots$legend.background <- ggplot2::element_rect(fill = colors[15], color = NA)
+  }
+
+  if (!"legend.margin" %in% names(dots)) {
+    dots$legend.margin <- ggplot2::margin(t = 0, r = 0, b = 0, l = 0)
+  }
+
+  if (!"legend.spacing" %in% names(dots)) {
+    dots$legend.spacing <- ggplot2::unit(10, "pt")
+  }
+
+  if (!"legend.spacing.x" %in% names(dots)) {
+    dots$legend.spacing.x <- NULL  # Inherits from legend.spacing
+  }
+
+  if (!"legend.spacing.y" %in% names(dots)) {
+    dots$legend.spacing.y <- NULL  # Inherits from legend.spacing
+  }
+
+  if (!"legend.key" %in% names(dots)) {
+    dots$legend.key <- ggplot2::element_rect(fill = colors[15], color = NA)
   }
 
   if (!"legend.key.size" %in% names(dots)) {
     dots$legend.key.size <- ggplot2::unit(20, "pt")
   }
 
-  if (!"legend.position" %in% names(dots)) {
-    dots$legend.position <- c(0.95, 1.035)
+  if (!"legend.key.height" %in% names(dots)) {
+    dots$legend.key.height <- NULL  # Inherits from legend.key.size
+  }
+
+  if (!"legend.key.width" %in% names(dots)) {
+    dots$legend.key.width <- NULL  # Inherits from legend.key.size
   }
 
   if (!"legend.text" %in% names(dots)) {
@@ -166,32 +269,94 @@ theme_pcj <- function(gridlines = TRUE, palette = "default",
     dots$legend.title.position <- "top"
   }
 
-  if (!"panel.grid.major" %in% names(dots)) {
-    dots$panel.grid.major <- gridlines
+  if (!"legend.position" %in% names(dots)) {
+    dots$legend.position <- c(0.95, 1.035)
   }
 
-  if (!"panel.grid.minor" %in% names(dots)) {
-    dots$panel.grid.minor <- ggplot2::element_blank()
+  if (!"legend.direction" %in% names(dots)) {
+    dots$legend.direction <- "horizontal"
+  }
+
+  if (!"legend.justification" %in% names(dots)) {
+    dots$legend.justification <- c(1, 0)
+  }
+
+  if (!"legend.box" %in% names(dots)) {
+    dots$legend.box <- NULL  # Default is "vertical"
+  }
+
+  if (!"legend.box.just" %in% names(dots)) {
+    dots$legend.box.just <- NULL  # Default is "top"
+  }
+
+  if (!"legend.box.margin" %in% names(dots)) {
+    dots$legend.box.margin <- ggplot2::margin(t = 5, r = 25, b = 0, l = 25)
+  }
+
+  if (!"legend.box.background" %in% names(dots)) {
+    dots$legend.box.background <- ggplot2::element_rect(linewidth = 1)
+  }
+
+  if (!"legend.box.spacing" %in% names(dots)) {
+    dots$legend.box.spacing <- ggplot2::unit(6, "pt")
+  }
+
+  # ========== PANEL ELEMENTS ==========
+  if (!"panel.background" %in% names(dots)) {
+    dots$panel.background <- ggplot2::element_rect(fill = colors[15], color = NA)
+  }
+
+  if (!"panel.border" %in% names(dots)) {
+    dots$panel.border <- ggplot2::element_blank()
   }
 
   if (!"panel.spacing" %in% names(dots)) {
     dots$panel.spacing <- ggplot2::unit(.5, units = "in")
   }
 
+  if (!"panel.spacing.x" %in% names(dots)) {
+    dots$panel.spacing.x <- NULL  # Inherits from panel.spacing
+  }
+
+  if (!"panel.spacing.y" %in% names(dots)) {
+    dots$panel.spacing.y <- NULL  # Inherits from panel.spacing
+  }
+
+  if (!"panel.grid" %in% names(dots)) {
+    dots$panel.grid <- NULL  # Inherits from panel.grid.major and panel.grid.minor
+  }
+
+  if (!"panel.grid.major" %in% names(dots)) {
+    dots$panel.grid.major <- gridlines
+  }
+
+  if (!"panel.grid.major.x" %in% names(dots)) {
+    dots$panel.grid.major.x <- NULL  # Inherits from panel.grid.major
+  }
+
+  if (!"panel.grid.major.y" %in% names(dots)) {
+    dots$panel.grid.major.y <- NULL  # Inherits from panel.grid.major
+  }
+
+  if (!"panel.grid.minor" %in% names(dots)) {
+    dots$panel.grid.minor <- ggplot2::element_blank()
+  }
+
+  if (!"panel.grid.minor.x" %in% names(dots)) {
+    dots$panel.grid.minor.x <- NULL  # Inherits from panel.grid.minor
+  }
+
+  if (!"panel.grid.minor.y" %in% names(dots)) {
+    dots$panel.grid.minor.y <- NULL  # Inherits from panel.grid.minor
+  }
+
+  if (!"panel.ontop" %in% names(dots)) {
+    dots$panel.ontop <- FALSE
+  }
+
+  # ========== PLOT ELEMENTS ==========
   if (!"plot.background" %in% names(dots)) {
-    dots$plot.background <- ggplot2::element_rect(fill = colors[15])
-  }
-
-  if (!"plot.caption" %in% names(dots)) {
-    dots$plot.caption <- ggplot2::element_text(color = colors[11], hjust = 1)
-  }
-
-  if (!"plot.caption.position" %in% names(dots)) {
-    dots$plot.caption.position <- "plot"
-  }
-
-  if (!"plot.margin" %in% names(dots)) {
-    dots$plot.margin <- ggplot2::margin(t = .15, r = .25, b = .15, l = .25, unit = "in")
+    dots$plot.background <- ggplot2::element_rect(fill = colors[15], color = NA)
   }
 
   if (!"plot.title" %in% names(dots)) {
@@ -216,16 +381,97 @@ theme_pcj <- function(gridlines = TRUE, palette = "default",
     )
   }
 
+  if (!"plot.caption" %in% names(dots)) {
+    dots$plot.caption <- ggplot2::element_text(color = colors[11], hjust = 1)
+  }
+
+  if (!"plot.caption.position" %in% names(dots)) {
+    dots$plot.caption.position <- "plot"
+  }
+
+  if (!"plot.tag" %in% names(dots)) {
+    dots$plot.tag <- ggplot2::element_text(
+      size = ggplot2::rel(1.5),
+      hjust = 0.5,
+      vjust = 0.5
+    )
+  }
+
+  if (!"plot.tag.position" %in% names(dots)) {
+    dots$plot.tag.position <- "topleft"
+  }
+
+  if (!"plot.margin" %in% names(dots)) {
+    dots$plot.margin <- ggplot2::margin(t = .15, r = .25, b = .15, l = .25, unit = "in")
+  }
+
+  # ========== STRIP ELEMENTS (for facets) ==========
   if (!"strip.background" %in% names(dots)) {
-    dots$strip.background <- ggplot2::element_rect(fill = colors[13])
+    dots$strip.background <- ggplot2::element_rect(fill = colors[13], color = NA)
+  }
+
+  if (!"strip.background.x" %in% names(dots)) {
+    dots$strip.background.x <- NULL  # Inherits from strip.background
+  }
+
+  if (!"strip.background.y" %in% names(dots)) {
+    dots$strip.background.y <- NULL  # Inherits from strip.background
+  }
+
+  if (!"strip.clip" %in% names(dots)) {
+    dots$strip.clip <- "inherit"
   }
 
   if (!"strip.placement" %in% names(dots)) {
     dots$strip.placement <- "inside"
   }
 
+  if (!"strip.text" %in% names(dots)) {
+    dots$strip.text <- ggplot2::element_text(
+      size = ggplot2::rel(1.2),
+      color = colors[1],
+      margin = ggplot2::margin(t = 5, r = 5, b = 5, l = 5)
+    )
+  }
+
+  if (!"strip.text.x" %in% names(dots)) {
+    dots$strip.text.x <- NULL  # Inherits from strip.text
+  }
+
+  if (!"strip.text.x.top" %in% names(dots)) {
+    dots$strip.text.x.top <- NULL  # Inherits from strip.text.x
+  }
+
+  if (!"strip.text.x.bottom" %in% names(dots)) {
+    dots$strip.text.x.bottom <- NULL  # Inherits from strip.text.x
+  }
+
+  if (!"strip.text.y" %in% names(dots)) {
+    dots$strip.text.y <- NULL  # Inherits from strip.text
+  }
+
+  if (!"strip.text.y.left" %in% names(dots)) {
+    dots$strip.text.y.left <- NULL  # Inherits from strip.text.y
+  }
+
+  if (!"strip.text.y.right" %in% names(dots)) {
+    dots$strip.text.y.right <- NULL  # Inherits from strip.text.y
+  }
+
+  if (!"strip.switch.pad.grid" %in% names(dots)) {
+    dots$strip.switch.pad.grid <- ggplot2::unit(3, "pt")
+  }
+
+  if (!"strip.switch.pad.wrap" %in% names(dots)) {
+    dots$strip.switch.pad.wrap <- ggplot2::unit(3, "pt")
+  }
+
+  # ========== OTHER ELEMENTS ==========
+  if (!"aspect.ratio" %in% names(dots)) {
+    dots$aspect.ratio <- NULL  # No fixed aspect ratio by default
+  }
+
   # Apply the theme with all defaults and user overrides
   ggplot2::theme_void() %+replace%
     do.call(ggplot2::theme, dots)
-
 }
